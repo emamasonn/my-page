53c6553b318bd73f1b2239d6f5e68e759e24db11
David Mason <emamasonn@gmail.com>
2023-04-13 15:43:06 -0300

    change in components folder

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -3,16 +3,16 @@ import { motion } from "framer-motion";
 import Markdown from "markdown-to-jsx";
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Code from "../components/Code";
-import TitleH3 from "../components/TitleH3";
-import TitleH5 from "../components/TitleH5";
-import Note from "../components/Note";
-import UnorderedList from "../components/UnorderedList";
-import Section from "../components/Section";
-import TitleH1 from "../components/TitleH1";
-import MainImage from "../components/MainImage";
+import Code from "../components/markdown/Code";
+import TitleH3 from "../components/markdown/TitleH3";
+import TitleH5 from "../components/markdown/TitleH5";
+import Note from "../components/markdown/Note";
+import UnorderedList from "../components/markdown/UnorderedList";
+import Section from "../components/markdown/Section";
+import TitleH1 from "../components/markdown/TitleH1";
+import MainImage from "../components/markdown/MainImage";
 
-import Image from "../components/Image";
+import Image from "../components/markdown/Image";
 import { I18nProviderContext } from "../intl/IntlProvider";
 
 const options = {
