f9cc30fe9320b812ed1c97020a3b50bc398f3a2f
David Mason <emamasonn@gmail.com>
2023-04-13 23:48:40 -0300

    change text in About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -29,7 +29,6 @@ const About = () => {
         animate="visible"
       >
         <Heading>About Me</Heading>
-
         <Flex
           mt="30px"
           justifyContent="space-between"
@@ -38,58 +37,54 @@ const About = () => {
         >
           <Flex flexDirection="column" maxW="600px" mb="30px">
             <Text mb="20px">
-              Hi, I'm Emanuel Mason and I want you to get to know me a little
-              bit better and tell you about how I ended up working as a
-              developer.
+              Hi, my name is David Mason, I'm a Front-end Developer and I really
+              enjoy using technology to create solutions.
             </Text>
             <Text mb="20px">
-              When I moved to La Plata to study Computer Engineering I had to
-              look for a job, that led me to work as a dishwasher, kitchen
-              helper or as a builder.
+              My journey in development started when I needed a change of job
+              and I was studying Computer Engineering, then some friends
+              motivated me to start web development.
               <br />
-              But that had nothing to do with programming and that bothered me.
-              I have some friends who at that time worked as developers, that
-              motivated me to start studying on my own and after a few hard
-              months I was able to get my first job as a developer and from then
-              on I did not stop growing professionally.
+              Since then, in every job I had as a developer I was able to learn
+              a lot together with a great team that made the process more fun.
             </Text>
             <Text mb="20px">
-              Since then I had several jobs as a developer and I can say that in
-              each one I learned a lot and I was lucky that I always had a great
-              team.
-            </Text>
-            <Text mb="20px">
-              I worked in different types of companies such as a start up, a
-              large company and as a freelance so I had in my hands varied
-              projects from creating an application from scratch to work on a
-              finished product.
-            </Text>
-            <Text mb="20px">
-              Not everything is development, I also love other things like
-              outdoor activities such as trekking or just having some mates in a
-              park, BJJ and making a barbecue with family or friends.
+              Now, my focus is to continue polishing my Front-end skills and
+              expanding my Back-end knowledge.
             </Text>
             <Text mb="20px">
               Here are a few technologies I’ve been working with recently:
             </Text>
-            <UnorderedList listStyleType="none" margin="0" fontSize="sm">
-              <ListItem>
-                <ListIcon as={FaHandPointRight} color="#fc0283" />
-                JavaScript (ES6+)
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaHandPointRight} color="#fc0283" />
-                TypeScript
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaHandPointRight} color="#fc0283" />
-                ReactJS
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaHandPointRight} color="#fc0283" />
-                ExpressJS
-              </ListItem>
-            </UnorderedList>
+            <Flex justifyContent="flex-start" alignItems="center">
+              <UnorderedList listStyleType="none" margin="20px" fontSize="sm">
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  JavaScript (ES6+)
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  TypeScript
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  ReactJS
+                </ListItem>
+              </UnorderedList>
+              <UnorderedList listStyleType="none" margin="20px" fontSize="sm">
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  ExpressJS
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  MongoDB
+                </ListItem>
+                <ListItem>
+                  <ListIcon as={FaHandPointRight} color="#fc0283" />
+                  Firebase
+                </ListItem>
+              </UnorderedList>
+            </Flex>
           </Flex>
           <Flex justifyContent="center" w={["100%", "100%", "100%", "unset"]}>
             <Flex position="relative">
